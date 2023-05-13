a42b2658781b7f5ebd2e7466e1256933d6365d8e
Sebastian Lopez <sebas.lopezsilva147@gmail.com>
2023-05-13 16:50:56 -0500

    Se agregan metodos getRoot, LeftDescendant, RightAncestor (No completo), find y next

--- ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/datastructures/nonlinear/BinarySearchTree.java
+++ ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/datastructures/nonlinear/BinarySearchTree.java
@@ -1,13 +1,13 @@
 package com.pensumeditor.datastructures.nonlinear;
 
-public class BinarySearchTree <T> {
+public class BinarySearchTree {
     class Node {
-        private T data;
+        private int key;
         private Node left;
         private Node right;
 
-        public Node(T data) {
-            this.data = data;
+        public Node(int key) {
+            this.key = key;
             left = null;
             right = null;
         }
@@ -15,9 +15,56 @@ public class BinarySearchTree <T> {
 
     private Node root;
 
-    public BinarySearchTree(T data) {
-        root = new Node(data);
+    public BinarySearchTree(int key) {
+        root = new Node(key);
     }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public Node LeftDescendant(Node node) {
+        if (node.left != null) {
+            return LeftDescendant(node.left);
+        } else {
+            return node;
+        }
+    }
+
+    public Node RightAncestor(Node node) { // Revisar si sirve
+        if (root != null) {
+            Node actual = root;
+            while (actual.left.key > node.key) {
+                actual = actual.left;
+            }
+            return actual;
+        }
+        return null;
+    }
+
+    public Node find(int value, Node node) {
+        if (node.key == value) {
+            return node;
+        } else if (node.key > value) {
+            if (node.left != null) {
+                return find(value, node.left);
+            }
+            return node;
+        } else {
+            return find(value, node.right);
+        }
+    }
+
+    public Node next(Node node) {
+        if (node.right != null) {
+            return LeftDescendant(node.right);
+        } else {
+            return RightAncestor(node);
+        }
+
+    }
+
+
 
 }
 
