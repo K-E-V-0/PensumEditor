7ccbac727694531dcccf0df589f51ffa930a1219
Sebastian Lopez <sebas.lopezsilva147@gmail.com>
2023-05-19 22:09:46 -0500

    BinarySearchTree incompleto y Heap totalmente funcional

--- ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/datastructures/nonlinear/BinarySearchTree.java
+++ ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/datastructures/nonlinear/BinarySearchTree.java
@@ -1,15 +1,18 @@
 package com.pensumeditor.datastructures.nonlinear;
 
 public class BinarySearchTree {
-    class Node {
+    public class Node {
         private int key;
         private Node left;
         private Node right;
 
         public Node(int key) {
             this.key = key;
-            left = null;
-            right = null;
+            left = right = null;
+        }
+
+        public int getKey() {
+            return key;
         }
     }
 
@@ -17,6 +20,9 @@ public class BinarySearchTree {
 
     public BinarySearchTree(int key) {
         root = new Node(key);
+    }
+    public BinarySearchTree() {
+        root = null;
     }
 
     public Node getRoot() {
@@ -42,17 +48,69 @@ public class BinarySearchTree {
         return null;
     }
 
-    public Node find(int value, Node node) {
-        if (node.key == value) {
-            return node;
-        } else if (node.key > value) {
-            if (node.left != null) {
-                return find(value, node.left);
-            }
-            return node;
-        } else {
-            return find(value, node.right);
+    public Node search(int value, Node node) {
+        Node nextNode = node.left;
+        if (node.key < value) {
+            nextNode = node.right;
         }
+        if (nextNode != null) {
+            return search(value, nextNode);
+        }
+        return node;
+    }
+
+    public Node search(int value) {
+        return search(value, root);
+    }
+
+    public Node insert(int key) {
+        Node newNode = new Node(key);
+        if (root == null) {
+            root = newNode;
+            return root;
+        }
+        Node insertNode = search(key);
+        if (insertNode.key == key) {
+            inOrderTraversal();
+            return insertNode;
+        }
+        if (insertNode.key > key) {
+            insertNode.left = newNode;
+        } else {
+            insertNode.right = newNode;
+        }
+        return newNode;
+    }
+
+    public void delete(int key) {
+        root = delete(root, key);
+    }
+
+    public Node delete(Node root, int key) {
+        // Return if the tree is empty
+        /*if (root == null)
+            return root;
+
+        // Find the node to be deleted
+        if (key < root.key)
+            root.left = delete(root.left, key);
+        else if (key > root.key)
+            root.right = delete(root.right, key);
+        else {
+            // If the node is with only one child or no child
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            // If the node has two children
+            // Place the inorder successor in position of the node to be deleted
+            root.key = minValue(root.right);
+
+            // Delete the inorder successor
+            root.right = delete(root.right, root.key);
+        }*/
+        return root;
     }
 
     public Node next(Node node) {
@@ -64,7 +122,41 @@ public class BinarySearchTree {
 
     }
 
+    public void inOrderTraversal() {
+        inOrderTraversal(root);
+        System.out.println();
+    }
+    public void inOrderTraversal(Node node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.print(node.key + " ");
+            inOrderTraversal(node.right);
+        }
+    }
 
+    public void preOrderTraversal() {
+        preOrderTraversal(root);
+        System.out.println();
+    }
+    public void preOrderTraversal(Node node) {
+        if (node != null) {
+            System.out.print(node.key + " ");
+            inOrderTraversal(node.left);
+            inOrderTraversal(node.right);
+        }
+    }
+
+    public void postOrderTraversal() {
+        postOrderTraversal(root);
+        System.out.println();
+    }
+    public void postOrderTraversal(Node node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            inOrderTraversal(node.right);
+            System.out.print(node.key + " ");
+        }
+    }
 
 }
 
--- ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/main/Main.java
+++ ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/main/Main.java
@@ -1,13 +1,38 @@
 package com.pensumeditor.main;
 
+import com.pensumeditor.datastructures.nonlinear.BinarySearchTree;
+import com.pensumeditor.datastructures.nonlinear.BinarySearchTree.*;
+import com.pensumeditor.datastructures.nonlinear.Heap;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        UserInterface userInterface = new UserInterface();
+        /*UserInterface userInterface = new UserInterface();
 
-        userInterface.startMenu();
+        userInterface.startMenu();*/
 
+        Heap heaps = new Heap(20);
+        heaps.insert(5);
+        heaps.insert(8);
+        heaps.insert(1);
+        heaps.insert(4);
+        heaps.insert(2);
+        heaps.insert(6);
+        heaps.insert(9);
+        heaps.insert(8);
+        heaps.insert(3);
+        heaps.insert(6);
+        heaps.insert(9);
+        heaps.insert(25);
+        heaps.insert(34);
+        heaps.insert(12);
+        heaps.insert(84);
+        heaps.insert(63);
+        heaps.insert(71);
+        System.out.println(heaps);
+        System.out.println(heaps.extractMax());
+        System.out.println(heaps);
     }
 
 }
