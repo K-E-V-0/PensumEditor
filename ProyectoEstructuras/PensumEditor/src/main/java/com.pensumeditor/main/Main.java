bfa0c981e0f825d5cd6635a0e9ea7598a6b1ed2f
Javier-Carrillo-12 <javierandrescarrillo12@gmail.com>
2023-05-27 22:17:48 -0500

    Prueba

--- ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/main/Main.java
+++ ProyectoEstructuras/PensumEditor/src/main/java/com.pensumeditor/main/Main.java
@@ -7,28 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*UserInterface userInterface = new UserInterface();
+        UserInterface userInterface = new UserInterface();
 
-        userInterface.startMenu();*/
+        userInterface.startMenu();
+        System.out.println("kevvvvv");
 
-        Tree tree = new AVLTree();
-        tree.insert(5);
-        tree.insert(4);
-        tree.insert(6);
-        tree.insert(3);
-        tree.insert(7);
-        tree.insert(2);
-        tree.insert(8);
-        tree.printTree();
-        System.out.println(tree.findMin());
-        System.out.println(tree.findMax());
-        System.out.println(tree.height());
-        System.out.println(tree.size());
-        tree.inOrderTraversal();
-        tree.preOrderTraversal();
-        tree.postOrderTraversal();
-        System.out.println(tree.search(7));
-        System.out.println(tree.search(10));
-        System.out.println(tree.rangeSearch(0,10));
+
     }
 }
